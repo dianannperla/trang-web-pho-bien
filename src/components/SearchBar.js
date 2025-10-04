1a7f576c819d31b04ce058554e5a146f3605356e
dianannperla <waafirmelyza@hotmail.com>
2025-10-04 18:00:17 +0100

    feat: initial commit for src/components/SearchBar.js

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Tìm kiếm địa điểm..." />
+      <button>Tìm Kiếm</button>
+    </div>
+  );
+}
+
+export default SearchBar;
